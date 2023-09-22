a702b54b929727f491e4142cef4b91db9079d105
Lucas Baronio <lbaronio20@hotmail.com>
2023-09-22 20:54:00 -0300

    fix: change constants

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 export const GAME_BOARD_COLOR = 'aliceblue'
 export const SNAKE_COLOR = 'blue'
 export const FOOD_COLOR = 'burlywood'
-export const BOARD_SIZE = 21
-export const SNAKE_SPEED = 10
+export const BOARD_SIZE = 15
+export const SNAKE_SPEED = 7
 export const DIRECTION_TYPE = {
   LEFT: 'LEFT',
   RIGHT: 'RIGHT',
